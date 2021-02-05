1813b804a0f15a3c7639843748788e513a673326
Slavik Ohorodnik <teatrgsc@gmail.com>
2021-02-05 21:41:56 +0200

    sedGrid sender

--- api/auth/auth.controllers.js
+++ api/auth/auth.controllers.js
@@ -1,5 +1,8 @@
 const UsersModel = require("../users/users.model");
 const generateAvatar = require("../helpers/generateAvatar");
+const sendEmailVerification = require("../helpers/email.vetification.send");
+const { v4: uuidv4 } = require("uuid");
+
 require("dotenv").config();
 
 module.exports.registration = async (req, res) => {
@@ -10,13 +13,17 @@ module.exports.registration = async (req, res) => {
       .json({ message: "a user with this email is already exists" });
   }
   const passwordHashed = await UsersModel.hashedPassword(req.body.password);
+
   const avatar = await generateAvatar(req.body.email);
   const avatarUrl = `http://localhost:${process.env.PORT}/images/${avatar}`;
-  await UsersModel.create({
+  const user = await UsersModel.create({
     email: req.body.email,
     password: passwordHashed,
     avatarUrl,
   });
+  const verificationToken = uuidv4();
+  await user.createVerificationToken(verificationToken);
+  await sendEmailVerification(req.body.email, verificationToken);
   res
     .status(201)
     .json({ user: { email: req.body.email, subscription: "free" } });
@@ -34,7 +41,6 @@ module.exports.login = async (req, res) => {
   }
 
   const token = await userToCheck.sign();
-  // const token = await userToCheck.sign(userToCheck.email, userToCheck._id);
 
   res.status(200).json({
     token: token,
@@ -50,4 +56,17 @@ module.exports.logout = async (req, res) => {
   if (!user) return res.status(401).json({ message: "Not authorized" });
   user.token = null;
   res.status(204).send();
+};
+
+module.exports.verifyEmail = async (req, res) => {
+  const { verificationToken } = req.params;
+
+  const verifiedUser = await UsersModel.findByVerificationToken(
+    verificationToken
+  );
+  if (!verifiedUser) {
+    return res.send(404).send("User not found!");
+  }
+  await verifiedUser.removeVerificationToken();
+  return res.status(200).send("Verified");
 };
